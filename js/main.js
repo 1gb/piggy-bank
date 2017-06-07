024715880caec3b8daf8557a02fae52015aa1659
Eve Worthington <worthington.ee@gmail.com>
2017-06-06 21:44:16 -0700

    animating coin, restructuring layout, adding snap

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
-//style the sliders so they're bigger/easier to grab on mobile
+//style the sliders so they're bigger/easier to grab on mobile - I think this is done?
+// Centering the text within the wage boxes... messing with flexbox
+// Make the cumulative total appear in the middle of the pig!
 
 var secondsElapsed = 0;
 var minutesElapsed = 0;
 var cumulativeMoney = 0;
 var wagePerSecond, timingVar;
-
-
+var coin = Snap.select('#coin');
+var t = new Snap.Matrix();
+t.translate(0, 180);
+var t2 = new Snap.Matrix();
+t2.translate(0, 0);
 
 $( document ).ready(function() {
+
   $('.range-slider').on('input', function() {
+    //this resets the slider, timer, and money count
     var newValue = $(this).val();
     var label = $( '#' + $(this).attr('id') + '-display');
     var centsVal = $('#cents').val();
-
 
     clearInterval(timingVar);
     cumulativeMoney = 0;
@@ -36,29 +42,47 @@ $( document ).ready(function() {
 
   function counting() {
     var elapsedTime;
-
     cumulativeMoney = Number((cumulativeMoney += wagePerSecond).toFixed(4));
-
     $('#elapsedTime').html(elapsedTime);
     $('#cumulativeWage').html(cumulativeMoney);
   }
 
-  $('#startStopBtn').on('click', function(e) {
+  function startCoinAnimation() {
+    coin.transform(t2);
+    coin.animate({
+      transform: t
+    }, 1000, mina.bounce);
 
+    if ($('#startStopBtn').hasClass('startCounter')) {
+      coin.stop();
+      return;
+    }
+    setTimeout(startCoinAnimation, 1000);
+  }
+
+  function stopCoinAnimation() {
+    coin.transform(t2);
+  }
+
+
+  $('#startStopBtn').on('click', function(e) {
+    coin.stop();
     e.preventDefault();
     clearInterval(timingVar);
     cumulativeMoney = 0;
-    console.log(this);
     timingVar = window.setInterval(counting, 1000);
-    if ($('#startStopBtn').hasClass('startCounter')) {
-      $('html, body').animate({
-        scrollTop: $("#pigdiv").offset().top
-      }, 1000);
+    if ($('#startStopBtn').hasClass('startCounter')) { // start
+      // $('html, body').animate({
+      //   scrollTop: $("#pigdiv").offset().top
+      // }, 1000);
       $('#startStopBtn').removeClass('startCounter').addClass('stopCounter').text('Stop Counter');
-    } else {
+      startCoinAnimation();
+    } else { // stop
       $('#startStopBtn').removeClass('stopCounter').addClass('startCounter').text('Start Counter');
       clearInterval(timingVar);
       cumulativeMoney = 0;
+      coin.stop();
+      stopCoinAnimation();
     }
   });
 
